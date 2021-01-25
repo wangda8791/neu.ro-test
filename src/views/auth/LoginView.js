8512172364b98782e90ad517283a38dc4c72f290
Wang Da <wangda8791@163.com>
2021-01-25 21:31:18 +1000

    Add UI to LoginView

--- src/views/auth/LoginView.js
+++ src/views/auth/LoginView.js
@@ -1,7 +1,122 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import { Formik } from "formik";
+import {
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import Page from "../../components/Page";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    height: "100%",
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3),
+  },
+}));
 
 const LoginView = () => {
-  return <div>LoginView</div>;
+  const classes = useStyles();
+  const navigate = useNavigate();
+
+  return (
+    <Page className={classes.root} title="Login">
+      <Box
+        display="flex"
+        flexDirection="column"
+        height="100%"
+        justifyContent="center"
+      >
+        <Container maxWidth="sm">
+          <Formik
+            initialValues={{
+              email: "demo@neu.ro",
+              password: "Password123",
+            }}
+            validationSchema={Yup.object().shape({
+              email: Yup.string()
+                .email("Must be a valid email")
+                .max(255)
+                .required("Email is required"),
+              password: Yup.string().max(255).required("Password is required"),
+            })}
+            onSubmit={() => {
+              navigate("/app/jobs", { replace: true });
+            }}
+          >
+            {({
+              errors,
+              handleBlur,
+              handleChange,
+              handleSubmit,
+              isSubmitting,
+              touched,
+              values,
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <Box mb={3}>
+                  <Typography color="textPrimary" variant="h2">
+                    Sign in
+                  </Typography>
+                  <Typography
+                    color="textSecondary"
+                    gutterBottom
+                    variant="body2"
+                  >
+                    Sign in on the internal platform
+                  </Typography>
+                </Box>
+                <TextField
+                  error={Boolean(touched.email && errors.email)}
+                  fullWidth
+                  helperText={touched.email && errors.email}
+                  label="Email Address"
+                  margin="normal"
+                  name="email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  type="email"
+                  value={values.email}
+                  variant="outlined"
+                />
+                <TextField
+                  error={Boolean(touched.password && errors.password)}
+                  fullWidth
+                  helperText={touched.password && errors.password}
+                  label="Password"
+                  margin="normal"
+                  name="password"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  type="password"
+                  value={values.password}
+                  variant="outlined"
+                />
+                <Box my={2}>
+                  <Button
+                    color="primary"
+                    disabled={isSubmitting}
+                    fullWidth
+                    size="large"
+                    type="submit"
+                    variant="contained"
+                  >
+                    Sign in now
+                  </Button>
+                </Box>
+              </form>
+            )}
+          </Formik>
+        </Container>
+      </Box>
+    </Page>
+  );
 };
 
 export default LoginView;
